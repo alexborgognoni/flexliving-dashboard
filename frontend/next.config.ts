630552d0659a7fe0ff0724a6a75254cc54808975
Alex Borgognoni <alex.bor0419@gmail.com>
2025-08-23 22:41:23 +0200

    remove nextjs warnings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: '/home/alexb/Projects/FlexDashboard/code/frontend'
+  }
 };
 
 export default nextConfig;
